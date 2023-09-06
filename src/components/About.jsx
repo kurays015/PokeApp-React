7b29f66af7d8ccb5f893540aafa4582bea942820
Christzxz <christnarvarte15@gmail.com>
2023-09-06 19:03:41 +0800

    edit about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,16 @@
+import github from "../assets/GitHub-Mark.png";
 const About = () => {
   return (
     <div className="about">
-      <h1>Title</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex
-        dignissimos, nobis voluptates animi modi autem?
-      </p>
+      <h1>This PokeDexApp is made using pokeAPI by Christ Narvarte</h1>
+      <p>You can search and play a guessing game</p>
+      <a
+        href="https://github.com/kurays015/PokeApp-React"
+        target="_blank"
+        title="code"
+      >
+        <img src={github} width="50px" />
+      </a>
     </div>
   );
 };
